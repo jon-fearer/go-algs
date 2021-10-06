5029919111e9a49d2e1e4c5e084698f513f9bf2c
Jon Fearer <fearer.jon@gmail.com>
2021-10-06 12:22:20 -0600

    feat(algs): start merge sort

---
+++ internal/algorithms/mergesort.go
@@ -0,0 +1,9 @@
+package algorithms
+
+func MergeSort(data []int, low int, high int) ([]int, error) {
+	return nil, nil
+}
+
+func merge(first []int, second []int) ([]int, error) {
+	return nil, nil
+}
