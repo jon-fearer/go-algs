e9533ce6347e147fc81183100d098625d6642263
Jon Fearer <fearer.jon@gmail.com>
2021-10-06 20:57:16 -0600

    feat(algs): add merge sort

--- internal/algorithms/mergesort.go
+++ internal/algorithms/mergesort.go
@@ -1,9 +1,44 @@
 package algorithms
 
-func MergeSort(data []int, low int, high int) ([]int, error) {
-	return nil, nil
+func MergeSort(data []int) ([]int, error) {
+	left := 0
+	right := len(data) - 1
+	work := make([]int, len(data))
+	mergeSortSubset(&data, &work, left, right)
+	return data, nil
 }
 
-func merge(first []int, second []int) ([]int, error) {
-	return nil, nil
+func mergeSortSubset(data *[]int, work *[]int, left int, right int) {
+	if left < right {
+		middle := left + ((right - left) / 2)
+		mergeSortSubset(data, work, left, middle)
+		mergeSortSubset(data, work, middle + 1, right)
+		merge(data, work, left, middle, right)
+	}
+}
+
+func merge(data *[]int, work *[]int, left int, middle int, right int) {
+	for i := left; i <= right; i++ {
+		(*work)[i] = (*data)[i]
+	}
+
+	workLeft := left
+	workRight := middle + 1
+	currentIndex := left
+
+	for workLeft <= middle && workRight <= right {
+		if (*work)[workLeft] <= (*work)[workRight] {
+			(*data)[currentIndex] = (*work)[workLeft]
+			workLeft++
+		} else {
+			(*data)[currentIndex] = (*work)[workRight]
+			workRight++
+		}
+		currentIndex++
+	}
+
+	remaining := middle - workLeft
+	for i := 0; i <= remaining; i++ {
+		(*data)[currentIndex+ i] = (*work)[workLeft+ i]
+	}
 }
--- internal/algorithms/mergesort_test.go
+++ internal/algorithms/mergesort_test.go
@@ -1,1 +1,18 @@
 package algorithms
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	data := []int{9, 3, 8, 1, 8, 5, 4}
+	expected := []int{1, 3, 4, 5, 8, 8, 9}
+	result, err := MergeSort(data)
+	if err != nil {
+		t.Errorf("Merge sort error: %s", err.Error())
+	}
+	if reflect.DeepEqual(result, expected) == false {
+		t.Errorf("Lists do not match. Expected: %v, Actual: %v", expected, result)
+	}
+}
